5dbbb9e402c4299ea3480ab6d65dcee1b2839edc
umadevitenapalli <umadevitenapalli@gmail.com>
2025-06-23 23:12:24 +0530

    second commit

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/Features",
+    features = "src/test/resources/Features/productPage.feature",
     glue = {"com.stepdefs", "com.hooks"},
     plugin = {"pretty", "json:target/cucumber.json"},
     monochrome = true
