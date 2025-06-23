6367c084b90f889a1216ba49f1960ad6928126dc
umadevitenapalli <umadevitenapalli@gmail.com>
2025-06-23 23:16:36 +0530

    updated feature file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/Features/productPage.feature",
+    features = "src/test/resources/Features",
     glue = {"com.stepdefs", "com.hooks"},
     plugin = {"pretty", "json:target/cucumber.json"},
     monochrome = true
